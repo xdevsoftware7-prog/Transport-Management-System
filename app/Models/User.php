0ccf01de81d87c989344c7f031b27fa8cac4048c
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-20 07:07:45 +0100

    Ajout de HasRoles et les fillables de User dans le Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -20,8 +21,16 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'firstname',
+        'lastname',
+        'phone',
+        'civility',
+        'organisation',
+        'is_active',
+        'last_login_at',
     ];
 
     /**
