003b89652fa467e1ede3dd5a636d7137fd841ef6
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-20 09:03:18 +0100

    Configuration de laravel notify dans app.balde.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,9 @@
 
     {{-- Google Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@500&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@500&display=swap"
+        rel="stylesheet">
 
     {{-- Icons --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
@@ -17,33 +20,40 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @stack('styles')
+    {{-- laravel notify css --}}
+    @notifyCss
 </head>
+
 <body>
 
-{{-- ============================================================
+    {{-- ============================================================
      SIDEBAR
      Pour ajouter un item : copier un bloc <li> dans partials/sidebar.blade.php
      ============================================================ --}}
-@include('partials.sidebar')
+    @include('partials.sidebar')
 
-{{-- ============================================================
+    {{-- ============================================================
      MAIN CONTENT WRAPPER
      ============================================================ --}}
-<div class="main-wrapper" id="mainWrapper">
+    <div class="main-wrapper" id="mainWrapper">
 
-    {{-- Top bar --}}
-    @include('partials.topbar')
+        {{-- Top bar --}}
+        @include('partials.topbar')
 
-    {{-- Page Content --}}
-    <main class="page-content">
-        @yield('content')
-    </main>
+        {{-- Page Content --}}
+        <main class="page-content">
+            @yield('content')
+        </main>
 
-</div>
+    </div>
 
-{{-- App JS --}}
-<script src="{{ asset('js/app.js') }}"></script>
-@stack('scripts')
-
+    {{-- appel de laravel notify --}}
+    <x-notify::notify />
+    {{-- App JS --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('scripts')
+    {{-- laravel notify js --}}
+    @notifyJs
 </body>
+
 </html>
