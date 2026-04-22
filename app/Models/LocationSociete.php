1bc3f07979189ed737d995ecc7ce1152356e5eb0
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 15:59:13 +0100

    Ajout de model location Societe

--- app/Models/LocationSociete.php
+++ app/Models/LocationSociete.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LocationSociete extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom_societe',
+        'telephone',
+        'email',
+        'date_debut_contrat',
+        'date_fin_contrat',
+        'contrat_pdf',
+        'statut',
+        'notes'
+    ];
+    protected $casts = [
+        'date_debut_contrat'    => 'date',
+        'date_fin_contrat' => 'date',
+    ];
 }
