edfcee3a151f845452bf3ba6c1e663d8399539e3
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 22:36:29 +0100

    Activation de lien de semi-remorque

--- resources/views/autresLink/index.blade.php
+++ resources/views/autresLink/index.blade.php
@@ -22,7 +22,7 @@
             <div class="stat-mini"><a href="{{ route('villes.index') }}"><span>Villes</span></a></div>
             <div class="stat-mini"><a href="{{ route('articles.index') }}"><span>Articles</span></a></div>
             <div class="stat-mini"><a href="{{ route('location_societes.index') }}"><span>Location_Societes</span></a></div>
-            <div class="stat-mini"><a href="{{ route('clients.index') }}"><span>Clients</span></a></div>
+            <div class="stat-mini"><a href="{{ route('semi_remorques.index') }}"><span>Semi_Remorque</span></a></div>
         </div>
     </div>
 
