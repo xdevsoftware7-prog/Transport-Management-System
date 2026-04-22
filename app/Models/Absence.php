594ff9c38724aad1dfe8815620b868c4d693cf2f
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 23:53:37 +0100

    Ajout de model des absences

--- app/Models/Absence.php
+++ app/Models/Absence.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Absence extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'chauffeur_id',
+        'date_absence',
+        'heure_entree',
+        'heure_sortie',
+        'heures_sup',
+        'motif',
+    ];
+
+    protected $casts = [
+        'date_absence' => 'date',
+        'heures_sup'   => 'decimal:2',
+    ];
+
+    public function chauffeur()
+    {
+        return $this->belongsTo(Chauffeur::class);
+    }
 }
