217a8602914144a82508a00d9de952569c1c9e97
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 14:55:13 +0100

    Ajout de model  de client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'nom',
+        'type',
+        'email',
+        'telephone',
+        'adresse',
+        'ice',
+        'identifiant_fiscal',
+        'registre_commerce',
+        'statut_juridique',
+        'patente',
+        'num_cnss',
+        'modalite_paiement',
+        'is_active'
+    ];
 }
