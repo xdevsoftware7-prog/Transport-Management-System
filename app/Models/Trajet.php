2220977bfaeab5c85dba34e0d7960369e3ae73bb
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 23:18:00 +0100

    Ajout de model trajet

--- app/Models/Trajet.php
+++ app/Models/Trajet.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Trajet extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['ville_depart_id', 'ville_destination_id',    'adresse_depart',    'adresse_destination', 'distance_km', 'prix_autoroute', 'duree_minutes', 'statut'];
+
+    public function villeDepart()
+    {
+        return $this->belongsTo(Ville::class, 'ville_depart_id');
+    }
+    public function villeDestination()
+    {
+        return $this->belongsTo(Ville::class, 'ville_destination_id');
+    }
 }
