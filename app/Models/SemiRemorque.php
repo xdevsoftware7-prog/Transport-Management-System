1bdee3f704212d40139b3733e6efc518517554ae
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 22:36:00 +0100

    Ajout de model des semiremoque

--- app/Models/SemiRemorque.php
+++ app/Models/SemiRemorque.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SemiRemorque extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'matricule',
+        'marque',
+        'type_remorque',
+        'ptac',
+        'vin',
+        'is_active'
+    ];
 }
