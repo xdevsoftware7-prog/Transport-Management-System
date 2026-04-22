9abfe11ca7372f6f597d08e6426163546b153d27
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-22 14:55:35 +0100

    Ajout de model chauffeur

--- app/Models/Chauffeur.php
+++ app/Models/Chauffeur.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chauffeur extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'code_drv',
+        'nom',
+        'prenom',
+        'telephone',
+        'cin',
+        'date_exp_cin',
+        'cin_path',
+        'date_exp_permis',
+        'salaire_net',
+        'salaire_brut',
+        'statut',
+    ];
+
+    protected $casts = [
+        'date_exp_cin'    => 'date',
+        'date_exp_permis' => 'date',
+        'salaire_net'     => 'decimal:2',
+        'salaire_brut'    => 'decimal:2',
+    ];
+
+    // ── RELATIONS ──
+    public function permis()
+    {
+        return $this->hasMany(ChauffeurPermis::class);
+    }
 }
