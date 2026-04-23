be15250976045d7b10c71df2c5c4046b287aeed5
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-23 06:37:56 +0100

    Ajout de model de prime de deplacement

--- app/Models/PrimeDeplacement.php
+++ app/Models/PrimeDeplacement.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PrimeDeplacement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'prime_deplacements';
+
+    protected $fillable = [
+        'trajet_id',
+        'type_vehicule',
+        'montant_prime',
+    ];
+
+    protected $casts = [
+        'montant_prime' => 'decimal:2',
+    ];
+
+    /** Valeurs autorisées pour type_vehicule */
+    public const TYPES_VEHICULE = [
+        'tracteur',
+        'semi-remorque',
+        'camion',
+        'fourgon',
+        'benne',
+        'citerne',
+        'frigo',
+        'plateau',
+    ];
+
+    public function trajet()
+    {
+        return $this->belongsTo(Trajet::class);
+    }
 }
